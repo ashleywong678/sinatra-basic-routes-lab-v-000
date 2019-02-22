25b01e12a7cd2b5d7a1c476ffdfb68c9e007d484
Ashley Wong <ashwong04@gmail.com>
2019-02-22 23:02:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,11 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get '/name' do
-    "My name is Ashley"
+    "My name is Ashley."
   end
   
   get '/hometown' do
-    ""
+    "My hometown is Queens, NY."
   end
   
 end
